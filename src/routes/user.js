02852d7125419583d4fb6014a0b0f925e634a32c
Humberto Martins <82153962+humbertomartinsclic@users.noreply.github.com>
2021-05-18 11:36:58 -0300

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
-
 const user = require('../controllers/user.js');
 
-
+router.get('/api/status/',(req, res)=>{
+  res.json({"mensagem":"ok"});
+})
 
 router.get('/api/users/:id', user.getUserById);
-
 router.get('/api/users', user.getUsers);
-
 router.post('/api/users', user.newUser);
-
 router.delete('/users/:id', user.deleteUser);
-
 router.put('/api/users', user.updateUser);
-
 
 module.exports = router;
